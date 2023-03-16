86c5d902f62aa45e9dccc3136e180cb97276614b
Luis Fabrício De Llamas <luisfabricio@hotmail.com>
2023-03-15 21:19:29 -0300

    Criando o projeto

---
+++ src/main/java/com/ms/MIcroServiceEmail/MIcroServiceEmailApplication.java
@@ -0,0 +1,13 @@
+package com.ms.MIcroServiceEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIcroServiceEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MIcroServiceEmailApplication.class, args);
+	}
+
+}
