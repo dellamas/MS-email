2d9be9443d1490c04ea60df3680254bcfdaddfd7
dellamas <luisfabricio@hotmail.com.br>
2023-03-15 22:38:39 -0300

    -Criando a estrutura do micro serviço. -criando o banco de dados.

--- src/test/java/com/ms/MIcroServiceEmail/MIcroServiceEmailApplicationTests.java
+++ src/test/java/com/ms/MIcroServiceEmail/MIcroServiceEmailApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ms.MIcroServiceEmail;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MIcroServiceEmailApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ms.MIcroServiceEmail;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MIcroServiceEmailApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
